ff0f3e69a15393624c78031ee22fdac667044e02
Maggie Gong <meijic@outlook.com>
2020-01-18 21:59:30 +0800

    use cloudinary for scenarios images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_025839) do
+ActiveRecord::Schema.define(version: 2020_01_18_133834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_025839) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "imgurl"
   end
 
   create_table "steps", force: :cascade do |t|
